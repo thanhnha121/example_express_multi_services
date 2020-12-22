584942909550301ec35e782241d7c4e44684290f
Nha Hoang <nhaht94@gmail.com>
2020-12-22 10:33:40 +0700

    Create B

--- server.js
+++ server.js
@@ -3,18 +3,12 @@ const app = express();
 const port = 3000;
 
 app.get("/", (req, res) => {
-    res.send("This is service A2!");
+    res.send("This is service B!");
 });
 
 app.get("/info", (req, res) => {
     res.json({
-        name: 'Service A2',
-    });
-});
-
-app.get("/only_on_a", (req, res) => {
-    res.json({
-        title: "This api available only on A server!",
+        name: 'Service B',
     });
 });
 
