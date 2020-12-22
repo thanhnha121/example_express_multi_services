6a4d8b6671664facfd07a6b1d13d48c92fc091a8
Nha Hoang <nhaht94@gmail.com>
2020-12-22 10:30:21 +0700

    Create A2

--- server.js
+++ server.js
@@ -3,12 +3,12 @@ const app = express();
 const port = 3000;
 
 app.get("/", (req, res) => {
-    res.send("This is service A1!");
+    res.send("This is service A2!");
 });
 
 app.get("/info", (req, res) => {
     res.json({
-        name: 'Service A1',
+        name: 'Service A2',
     });
 });
 
